fa70555b7bf2a4d9d7a3de877b4bcc13c6821646
tomt00001 <107521427+tomt00001@users.noreply.github.com>
2024-10-14 14:36:13 -0500

    Add inventory report

--- p21api/reports.py
+++ p21api/reports.py
@@ -4,6 +4,7 @@ from .config import Config
 from .odata_client import ODataClient
 from .report_base import ReportBase
 from .report_daily_sales import ReportDailySales
+from .report_inventory import ReportInventory
 from .report_jarp import ReportJarp
 from .report_kennametal_pos import ReportKennametalPos
 from .report_monthly_consolidation import ReportMonthlyConsolidation
@@ -21,7 +22,9 @@ def do_reports(client: "ODataClient", config: Config) -> None:
             ReportMonthlyConsolidation,
             ReportJarp,
         ],
-        "inventory": [],
+        "inventory": [
+            ReportInventory,
+        ],
     }
     for report_class in report_groups.get(config.report_group, []):
         report = report_class(client=client, config=config)
