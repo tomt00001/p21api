54f09d7fc729f33a8c47192c444d4a20f6d0bbcf
Tom Thompson <107521427+tomt00001@users.noreply.github.com>
2025-02-01 20:27:21 -0600

    Remove reports file by moving logic into main function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from p21api.config import Config
 from p21api.odata_client import ODataClient
-from p21api.reports import do_reports
 
 from gui import show_gui_dialog
 
@@ -15,12 +14,33 @@ def main():
         merged_data = {**config.model_dump(), **data}
         config = Config.from_gui_input(merged_data)
 
+    assert config.base_url, "Base URL is required"
+    assert config.start_date, "Start date is required"
+
     if not config.has_login:
         raise ValueError("Username and password are required")
 
     client = ODataClient(config=config)
 
-    do_reports(client, config)
+    # Get the classes of each report in each report group
+    report_groups = config.get_report_groups()
+    report_classes = [
+        report
+        for report_group in [report_groups.get(x) for x in config.report_groups]
+        for report in report_group or []
+    ]
+
+    # Run each report from the list of classes
+    for report_class in report_classes:
+        report = report_class(
+            client=client,
+            start_date=config.start_date,
+            end_date=config.end_date,
+            output_folder=config.output_folder,
+            debug=config.debug,
+            config=config,
+        )
+        report.run()
 
 
 if __name__ == "__main__":
